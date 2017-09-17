43856e5dc91637a2de7f7c5e1866910c5b899f4f
Martin Kong <martinckong24@gmail.com>
2017-09-17 02:41:27 -0400

    working form

--- courseBuddy/views.py
+++ courseBuddy/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import NameForm
 
 
 def index(request):
@@ -8,3 +9,40 @@ def index(request):
 
 def details(request):
     return render(request, 'courseBuddy/details.html')
+
+def form(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'courseBuddy/form.html', {'form': form})
+
+def data(request):
+    newrow = ""
+    newrow += request.POST["identifier"] + ", "
+    newrow += request.POST["first_name"] + ", "
+    newrow += request.POST["last_name"] + ", "
+    newrow += request.POST["year_school"] + ", ["
+    newrow += request.POST["classes"].replace(',', '%') + "]\n"
+    print (newrow)
+    with open("courseBuddy/people_data.txt", "a+") as myfile:
+        myfile.write(newrow)
+        print ("done")
+    return render(request, 'courseBuddy/index.html')
+
+
+
+
+
+
